ce47571aaa87d05688714b6c971978a313bef2d0
Henri El Daher <henrieldaher@gmail.com>
2016-10-04 17:58:59 -0400

    correct error in login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,9 +7,11 @@ class UsersController < ApplicationController
   end
 
   def create
+    binding.pry
   	user = User.new(user_params)
   	if user.save
   	  session[:user_id] = user.id
+      redirect_to posts_path
   	else
   	  @errors = user.errors.full_messages
   	  render 'new'
--- db/migrate/20161004162550_create_posts.rb
+++ db/migrate/20161004162550_create_posts.rb
@@ -1,6 +1,9 @@
 class CreatePosts < ActiveRecord::Migration[5.0]
   def change
     create_table :posts do |t|
+      t.string :title, null: false
+      t.text :description, null: false
+      t.integer :user_id, null: false
 
       t.timestamps
     end
