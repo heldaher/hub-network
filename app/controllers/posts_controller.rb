2152277bc0e9a5c9b216084875bb1700bafc8227
Henri El Daher <henrieldaher@gmail.com>
2016-10-05 12:28:16 -0400

    progress to show posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,10 +4,29 @@ class PostsController < ApplicationController
   end
 
   def new
+  	# binding.pry
   	# require_user
   end
 
-  def show
-  	@post = Post.find(params[:id])
+  def create
+    user = current_user
+    post = user.posts.new(post_params)
+    if post.save
+      redirect_to post_path(post.id)
+    else
+      @errors = post.errors.full_messages
+      render 'new'
+    end
   end
+
+  def show
+  	binding.pry
+  	@post = Post.find(params[:id])
+  	@user = @post.user
+  end
+
+  private
+    def post_params
+      params.require(:post).permit(:title, :description, :user_id)
+    end
 end
