dd83bd986f5a2fdfce1a140d14593b9053a43f09
Henri El Daher <henrieldaher@gmail.com>
2016-11-13 09:24:36 -0500

    add prof pic

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,10 @@ class User < ApplicationRecord
   	# has_secure_password
 	has_many :posts
 
+  #add avatar (prof pic)
+  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
+
   # Not used with devise I think
 	# validates :username, :email, presence: true, uniqueness: true
 	# validates :password, length: { minimum: 6 }
