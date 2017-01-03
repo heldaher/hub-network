e28d92b711fca8e91d54e19af8e3316cec735d86
Henri El Daher <henrieldaher@gmail.com>
2017-01-02 23:07:34 -0500

    add first and last names to user sign up

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,11 +3,11 @@ class RegistrationsController < Devise::RegistrationsController
   private
 
   def sign_up_params
-  	params.require(:user).permit(:username, :email, :password, :password_confirmation, :avatar)
+  	params.require(:user).permit(:username, :firstname, :lastname, :email, :password, :password_confirmation, :avatar)
   end
 
   def account_update_params
-  	params.require(:user).permit(:username, :email, :password, :password_confirmation, :current_password, :avatar)
+  	params.require(:user).permit(:username, :firstname, :lastname, :email, :password, :password_confirmation, :current_password, :avatar)
   end
 
 end
